44f7c7c7012eb13796a990a3570d62ae7d1a0a2c
Aisha Olanrewaju <ishaolan1@gmail.com>
2025-05-20 13:34:07 +0100

    update

--- Day1.py
+++ Day1.py
@@ -6,3 +6,12 @@ elif num==0:
     print("Number is Zero")
 else:
     print("Number is Negative")
+    #Nested conditions 
+    age=21
+    if age>20:
+        if age <35: 
+            print("Young Adult")
+        else: 
+            print("Adult")    
+
+
