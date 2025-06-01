945ec39792a7ba899af5b1f6839b565f6a7fda31
Aisha Olanrewaju <ishaolan1@gmail.com>
2025-06-01 17:12:58 +0100

    update

---
+++ data_structures.py
@@ -0,0 +1,13 @@
+#List of integers
+numbers= [1,2,3,4,5,6]
+
+#List of strings
+names= ["Aisha", "Kabira", "Tolu", "Hummul"]
+
+#List of mixed data types
+mixed_list= [1, "Aisha", 3.14, True]
+
+print(mixed_list[-1])
+
+
+
